65c25346228f4703a6ea01d46c90b4c0449edfa2
Liz Vanderkloot <liz@lizvdk.com>
2016-01-07 16:12:30 -0500

    Add map

--- In da Haus/ViewControllers/ViewController.swift
+++ In da Haus/ViewControllers/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initMap()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func initMap() {
+        let initialLocation = CLLocation(latitude: 42.3670059, longitude: -71.0827051)
+        let regionRadius: CLLocationDistance = 1000
+        func centerMapOnLocation(location: CLLocation) {
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                regionRadius * 2.0, regionRadius * 2.0)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        centerMapOnLocation(initialLocation)
+    }
 
 }
 
