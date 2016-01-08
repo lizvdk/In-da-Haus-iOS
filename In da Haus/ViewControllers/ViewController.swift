e814d983ca4ed35fcfd235bf77a7be94f9bfb472
Liz Vanderkloot <liz@lizvdk.com>
2016-01-08 14:28:46 -0500

    Convert spaces to tabs

--- In da Haus/ViewControllers/ViewController.swift
+++ In da Haus/ViewControllers/ViewController.swift
@@ -15,30 +15,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 	
 	// MARK: Properties
 	
-    var locationManager = CLLocationManager()
-    @IBOutlet weak var mapView: MKMapView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	var locationManager = CLLocationManager()
+	@IBOutlet weak var mapView: MKMapView!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		
-        initLocationTracking()
+		initLocationTracking()
 		initRegionMonitoring()
-        initMap()
-        
-    }
+		initMap()
+		
+	}
 	
 	// MARK: Map
 	
-    func initMap() {
-        let initialLocation = CLLocation(latitude: 42.3673379, longitude: -71.0809888)
-        let regionRadius: CLLocationDistance = 500
-        func centerMapOnLocation(location: CLLocation) {
-            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                regionRadius * 2.0, regionRadius * 2.0)
-            mapView.setRegion(coordinateRegion, animated: true)
-        }
-        centerMapOnLocation(initialLocation)
-    }
+	func initMap() {
+		let initialLocation = CLLocation(latitude: 42.3673379, longitude: -71.0809888)
+		let regionRadius: CLLocationDistance = 500
+		func centerMapOnLocation(location: CLLocation) {
+			let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+				regionRadius * 2.0, regionRadius * 2.0)
+			mapView.setRegion(coordinateRegion, animated: true)
+		}
+		centerMapOnLocation(initialLocation)
+	}
 	
 	// MARK: Location Tracking
 	
@@ -57,45 +57,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 	
 	// MARK: CLLocationManagerDelegate
 	
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        mapView.showsUserLocation = (status == .AuthorizedAlways)
-    }
-    
-    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        print("didFailWithError: \(error.description)")
-        let alertController = UIAlertController(title: "Error", message: "We could not determine your location.", preferredStyle: .Alert)
-        
-        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
-        }
-        
-        alertController.addAction(OKAction)
-        self.presentViewController(alertController, animated: true) {
-        }
-    }
+	func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+		mapView.showsUserLocation = (status == .AuthorizedAlways)
+	}
 	
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    }
-    
-    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        print("Entered \(region.identifier)")
-        let alertController = UIAlertController(title: "Entered \(region.identifier)", message: "Tell Slack?", preferredStyle: .Alert)
-        
-        let okAction = UIAlertAction(title: "OK", style: .Default) { (action) in
-            self.sendMessageToSlack(region.identifier)
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
-        }
-        
-        alertController.addAction(okAction)
-        alertController.addAction(cancelAction)
-        self.presentViewController(alertController, animated: true) {
-        }
-    }
-    
-    func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
-        print("Monitoring \(region.identifier)")
-    }
+	func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+		print("didFailWithError: \(error.description)")
+		let alertController = UIAlertController(title: "Error", message: "We could not determine your location.", preferredStyle: .Alert)
+		
+		let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+		}
+		
+		alertController.addAction(OKAction)
+		self.presentViewController(alertController, animated: true) {
+		}
+	}
+	
+	func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+	}
+	
+	func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+		print("Entered \(region.identifier)")
+		let alertController = UIAlertController(title: "Entered \(region.identifier)", message: "Tell Slack?", preferredStyle: .Alert)
+		
+		let okAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+			self.sendMessageToSlack(region.identifier)
+		}
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+		}
+		
+		alertController.addAction(okAction)
+		alertController.addAction(cancelAction)
+		self.presentViewController(alertController, animated: true) {
+		}
+	}
+	
+	func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
+		print("Monitoring \(region.identifier)")
+	}
 	
 	func locationManager(manager: CLLocationManager,
 		monitoringDidFailForRegion region: CLRegion?,
@@ -104,14 +104,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 	}
 	
 	// MARK: Slack
-
-    func sendMessageToSlack(office: String) {
-        let parameters = [
-            "text": "Liz is at \(office)!",
-        ]
-        
-        Alamofire.request(.POST, "https://hooks.slack.com/services/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", parameters: parameters, encoding: .JSON)
-    }
-
+	
+	func sendMessageToSlack(office: String) {
+		let parameters = [
+			"text": "Liz is at \(office)!",
+		]
+		
+		Alamofire.request(.POST, "https://hooks.slack.com/services/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", parameters: parameters, encoding: .JSON)
+	}
+	
 }
 
